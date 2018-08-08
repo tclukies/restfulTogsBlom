bcd5207534c12f70be2a4c31e1a0d987644ecba3
Thomas Clukies <Thomas@Thomass-MacBook-Pro.local>
2018-08-08 11:51:00 -0600

    prepping for heroku deploy

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 8080;
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const expressSanitizer = require("express-sanitizer");
+console.log(expressSanitizer);
 
 //APP Config
-mongoose.connect("mongodb://localhost/restful_blog_app");
+// mongoose.connect("mongodb://localhost/restful_blog_app");
+mongoose.connect("mongodb://tom:password123@ds153841.mlab.com:53841/toms-blog");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(expressSanitizer());
 
 //MONGOOSE/MODEL/Config
 var blogSchema = new mongoose.Schema({
-  title: String,
-  image: String,
-  body: String,
-  created: { type: Date, default: Date.now }
+    title: String,
+    image: String,
+    body: String,
+    created: { type: Date, default: Date.now }
 });
 
 var Blog = mongoose.model("Blog", blogSchema);
@@ -28,48 +34,107 @@ var Blog = mongoose.model("Blog", blogSchema);
 //   body: "Hello this is a Blog Post"
 // });
 
+// app.get("/", function(req, res) {
+//   res.redirect("/blogs");
+// });
+
 app.get("/", function(req, res) {
-  res.redirect("/blogs");
+    res.render("home");
 });
 
 app.get("/blogs", function(req, res) {
-  Blog.find({}, function(err, blogs) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("index", { blogs: blogs });
-    }
-  });
+    Blog.find({}, function(err, blogs) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("index", { blogs: blogs });
+        }
+    });
 });
 
 //new route
 app.get("/blogs/new", function(req, res) {
-  res.render("new");
+    res.render("new");
 });
 
 //create route
 app.post("/blogs", function(req, res) {
-  //create blog then redirect to index
-  Blog.create(req.body.blog, function(err, newBlog) {
-    if (err) {
-      res.render("new");
-    } else {
-      res.redirect("blogs");
-    }
-  });
+    //create blog then redirect to index
+    console.log(req.body);
+    req.body.blog.body = req.sanitize(req.body.blog.body);
+    console.log("---------");
+    console.log(req.body);
+
+    Blog.create(req.body.blog, function(err, newBlog) {
+        if (err) {
+            res.render("new");
+        } else {
+            res.redirect("blogs");
+        }
+    });
 });
 
 //show route
 app.get("/blogs/:id", function(req, res) {
-  Blog.findById(req.params.id, function(err, foundBlog) {
-    if (err) {
-      res.redirect("/blogs");
-    } else {
-      res.render("show", { blog: foundBlog });
-    }
-  });
+    Blog.findById(req.params.id, function(err, foundBlog) {
+        if (err) {
+            res.redirect("/blogs");
+        } else {
+            res.render("show", { blog: foundBlog });
+        }
+    });
+});
+
+//edit route
+app.get("/blogs/:id/edit", function(req, res) {
+    Blog.findById(req.params.id, function(err, foundBlog) {
+        if (err) {
+            res.redirect("/blogs");
+        } else {
+            res.render("edit", { blog: foundBlog });
+        }
+    });
+});
+
+//update route
+app.put("/blogs/:id", function(req, res) {
+    req.body.blog.body = req.sanitize(req.body.blog.body);
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(
+        err,
+        updatedBlog
+    ) {
+        if (err) {
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs/" + req.params.id);
+        }
+    });
+});
+
+//delete route
+app.delete("/blogs/:id", function(req, res) {
+    Blog.findByIdAndRemove(req.params.id, function(err) {
+        if (err) {
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs");
+        }
+    });
+});
+
+//api routes
+app.get("/api", function(req, res) {
+    Blog.findById(req.params.id, function(err, foundBlog) {
+        if (err) {
+            res.redirect("/blogs");
+        } else {
+            blog => {
+                res.json({ blog });
+            };
+        }
+    });
 });
 
 app.listen(port, () => {
-  console.log("listening on port", port);
+    console.log("listening on port", port);
 });
